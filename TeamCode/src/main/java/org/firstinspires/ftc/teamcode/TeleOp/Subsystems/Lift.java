a3bb4631fa0359e6d2ba00ebd61c04f7dc7313eb
Keval Shah <57273556+kevalshah2005@users.noreply.github.com>
2022-09-24 18:21:35 -0400

    Wrote Lift state machine for scrimmage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Subsystems/Lift.java
@@ -11,16 +11,18 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @Config
 public class Lift {
     // Config parameters
-    public static double h1Original = 1;
-    public static double h2Original = 0;
+    public static double h1Retracted = 1;
+    public static double h2Retracted = 0;
     public static double h1Extended = 0.3;
     public static double h2Extended = 0.7;
-    public static double liftLow = 600;
-    public static double liftMid = 1600;
-    public static double liftHigh = 2600;
+    public static int liftLow = 600;
+    public static int liftMid = 1600;
+    public static int liftHigh = 2600;
     public static double grabPos = 1;
     public static double releasePos = 0.5;
     public static double waitTime = 0.5;
+    public static double desiredLiftPower = 1;
+    public static double minHeightForExtension = 400;
 
     private enum LiftState {
         START,
@@ -33,6 +35,24 @@ public class Lift {
 
     private LiftState liftState = LiftState.START;
 
+    /*
+    private enum DepositLocation {
+        LOW(liftLow),
+        MID(liftMid),
+        HIGH(liftHigh);
+
+        private static final DepositLocation[] states = values();
+
+        public int ticks;
+
+        DepositLocation(int ticks) {
+            this.ticks = ticks;
+        }
+    }
+
+    private DepositLocation depositLocation = DepositLocation.HIGH;
+    */
+
     private final Telemetry telemetry;
 
     private final DcMotorEx lift;
@@ -42,6 +62,9 @@ public class Lift {
 
     private final ElapsedTime eTime = new ElapsedTime();
 
+    private double liftPower;
+    private int depositTicks;
+
     public Lift(HardwareMap hardwareMap, Telemetry multipleTelemetry) {
         telemetry = multipleTelemetry;
 
@@ -50,17 +73,87 @@ public class Lift {
         horizontal2 = hardwareMap.get(ServoImplEx.class, "h2");
         grabber = hardwareMap.get(ServoImplEx.class, "grab");
 
+        lift.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         lift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         lift.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        horizontal1.setPosition(h1Retracted);
+        horizontal2.setPosition(h2Retracted);
     }
 
-    public void init() {
-        horizontal1.setPosition(h1Original);
-        horizontal2.setPosition(h2Original);
-
-    }
-
-    public void lift() {
+    public void lift(boolean liftButtonHigh, boolean liftButtonMid, boolean liftButtonLow,
+                     boolean depositButton, boolean cancelAutomation) {
         telemetry.addData("Lift State", liftState);
+        telemetry.addData("Lift Power", liftPower);
+        telemetry.addData("Lift Encoder Value", lift.getCurrentPosition());
+        telemetry.update();
+
+        switch (liftState) {
+            case START:
+                if (liftButtonHigh) {
+                    depositTicks = liftHigh;
+                    lift.setTargetPosition(depositTicks);
+
+                    lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    liftState = LiftState.LIFT;
+                } else if (liftButtonMid) {
+                    depositTicks = liftMid;
+                    lift.setTargetPosition(depositTicks);
+
+                    lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    liftState = LiftState.LIFT;
+                } else if (liftButtonLow) {
+                    depositTicks = liftLow;
+                    lift.setTargetPosition(depositTicks);
+
+                    lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    liftState = LiftState.LIFT;
+                }
+                break;
+
+            case LIFT:
+                liftPower = desiredLiftPower;
+                if (lift.getCurrentPosition() > minHeightForExtension) {
+                    liftState = LiftState.EXTEND;
+                }
+                break;
+
+            case EXTEND:
+                horizontal1.setPosition(h1Extended);
+                horizontal2.setPosition(h2Extended);
+                if (lift.getCurrentPosition() - depositTicks < 20) {
+                    if (depositButton) {
+                        liftPower = 0;
+                        eTime.reset();
+                        liftState = LiftState.DEPOSIT;
+                    }
+                }
+                break;
+
+            case DEPOSIT:
+                grabber.setPosition(releasePos);
+                if (eTime.time() > waitTime) {
+                    grabber.setPosition(grabPos);
+                    liftState = LiftState.RETRACT;
+                }
+                break;
+
+            case RETRACT:
+                lift.setTargetPosition(0);
+                liftPower = desiredLiftPower;
+                if (lift.getCurrentPosition() < 10) {
+                    liftState = LiftState.STOP;
+                }
+                break;
+
+            case STOP:
+                liftPower = 0;
+                liftState = LiftState.START;
+                break;
+        }
+
+        if (cancelAutomation) {
+            liftState = LiftState.STOP;
+        }
     }
 }
