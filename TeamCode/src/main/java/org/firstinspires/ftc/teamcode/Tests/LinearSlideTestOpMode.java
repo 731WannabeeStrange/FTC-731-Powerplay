c2d8499f0d7d23980ff406d56dff8b7c280cde59
Keval Shah <57273556+kevalshah2005@users.noreply.github.com>
2022-10-12 23:06:34 -0400

    Create LinearSlideTestOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LinearSlideTestOpMode.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp
+@Config
+public class LinearSlideTestOpMode extends LinearOpMode
+{
+    private DcMotorEx slide;
+    
+    private double slidePower = 0;
+
+    @Override
+    public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        slide = hardwareMap.get(DcMotorEx.class, "slide");
+
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.update();
+
+        //Wait for the start button to be pressed.
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        while (opModeIsActive()) {
+            slidePower = gamepad1.right_trigger - gamepad1.left_trigger;
+            slide.setPower(slidePower);
+
+            telemetry.addData("slide position", slide.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
