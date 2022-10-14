eaa6cd9dcef657dd97bc4bf2d558a5f8df1e6728
Keval Shah <57273556+kevalshah2005@users.noreply.github.com>
2022-10-14 14:24:06 -0400

    Deleted unused imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LinearSlideTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LinearSlideTestOpMode.java
@@ -3,18 +3,10 @@ package org.firstinspires.ftc.teamcode.Tests;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp
 @Config
