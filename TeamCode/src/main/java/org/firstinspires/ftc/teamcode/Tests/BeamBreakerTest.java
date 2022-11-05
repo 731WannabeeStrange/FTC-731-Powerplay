b4fb467b8ed32ce0a1d1bbbefcd22e136015712f
Keval Shah <57273556+kevalshah2005@users.noreply.github.com>
2022-11-04 23:35:44 -0400

    Changed telemetry for beam breaker test program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/BeamBreakerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/BeamBreakerTest.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp(name = "Beam Breaker Test", group = "Sensor")
 public class BeamBreakerTest extends LinearOpMode {
 
@@ -12,16 +14,17 @@ public class BeamBreakerTest extends LinearOpMode {
     @Override
     public void runOpMode() {
         beamBreaker = hardwareMap.get(DigitalChannel.class, "beamBreaker");
-
         beamBreaker.setMode(DigitalChannel.Mode.INPUT);
+
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.HTML);
 
         waitForStart();
 
         while (opModeIsActive()) {
             if (beamBreaker.getState()) {
-                telemetry.addData("Beam Breaker", "Beam Broken");
+                telemetry.addData("Beam Breaker getState()", "<p style=\"color:rgb(255,0,0);\">TRUE</p>");
             } else {
-                telemetry.addData("Digital Touch", "Beam Not Broken");
+                telemetry.addData("Beam Breaker getState()", "<p style=\"color:rgb(255,0,0);\">TRUE</p>");
             }
 
             telemetry.update();
