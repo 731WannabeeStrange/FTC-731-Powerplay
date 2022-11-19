281eb20421cc371759d987ce270db442a4f6d520
Keval <kevals2005@gmail.com>
2022-11-18 20:29:20 -0500

    Create PoleObserverTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/PoleObserverTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.vision.pole.PoleObserverPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+public class PoleObserverTest extends LinearOpMode {
+
+    PoleObserverPipeline pipeline;
+    OpenCvInternalCamera phoneCam;
+
+    public void OpenCVSetup() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(
+                OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        pipeline = new PoleObserverPipeline();
+        phoneCam.setPipeline(pipeline);
+
+        // Optimized so the preview isn't messed up
+        phoneCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                phoneCam.startStreaming(800,448, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("FAILED; Error Code", errorCode);
+                telemetry.update();
+            }
+        });
+
+        FtcDashboard.getInstance().startCameraStream(phoneCam, 0);
+    }
+
+    @Override
+    public void runOpMode() {
+        OpenCVSetup();
+        waitForStart();
+        while (opModeIsActive());
+    }
+}
