e4d107b3040f5525ae0628fe51adbddc093c1fae
Keval Shah <57273556+kevalshah2005@users.noreply.github.com>
2022-12-07 15:21:17 -0500

    Created scrimmage teleop for 11 December scrimmage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ScrimmageTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ScrimmageTeleOp.java
@@ -1,21 +1,24 @@
-/*
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Lift;
+import org.firstinspires.ftc.teamcode.robot.subsystems.ScoringMech;
 
 @TeleOp
 public class ScrimmageTeleOp extends LinearOpMode {
     Drivetrain dt;
-    Lift lift;
+    ScoringMech scoring;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        MultipleTelemetry multipleTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         dt = new Drivetrain(hardwareMap, telemetry);
-        lift = new Lift(hardwareMap, telemetry);
+        scoring = new ScoringMech(hardwareMap, multipleTelemetry);
 
         waitForStart();
 
@@ -25,22 +28,25 @@ public class ScrimmageTeleOp extends LinearOpMode {
                     gamepad1.left_stick_x,
                     gamepad1.right_stick_x,
                     gamepad1.left_bumper,
-                    gamepad1.right_bumper,
                     gamepad1.dpad_up,
                     gamepad1.dpad_left,
                     gamepad1.dpad_down,
                     gamepad1.dpad_right
             );
 
-            lift.lift(
+            scoring.score(
+                    gamepad1.right_trigger,
+                    gamepad1.left_trigger,
+                    gamepad1.right_bumper,
+                    gamepad1.left_bumper,
                     gamepad1.y,
                     gamepad1.b,
                     gamepad1.a,
                     gamepad1.x,
-                    gamepad1.right_bumper,
-                    gamepad1.left_trigger > 0 && gamepad1.right_trigger > 0
+                    gamepad2.right_stick_y,
+                    gamepad2.right_stick_x,
+                    gamepad1.back
             );
         }
     }
 }
-*/
