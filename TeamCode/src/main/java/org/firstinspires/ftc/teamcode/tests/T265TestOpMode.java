8f47bdb8d2bf75c45f42d6cbebbf06ffe72cf802
Ari Singh <arisingh8@gmail.com>
2023-01-13 18:36:54 -0500

    funny t265 attempt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/T265TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/T265TestOpMode.java
@@ -1,68 +1,46 @@
 package org.firstinspires.ftc.teamcode.tests;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.arcrobotics.ftclib.geometry.Pose2d;
-import com.arcrobotics.ftclib.geometry.Rotation2d;
-import com.arcrobotics.ftclib.geometry.Transform2d;
-import com.arcrobotics.ftclib.geometry.Translation2d;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.spartronics4915.lib.T265Camera;
+import com.spartronics4915.lib.T265Helper;
+
 
 @TeleOp
 @Config
-public class T265TestOpMode extends OpMode
+public class T265TestOpMode extends LinearOpMode
 {
-    // We treat this like a singleton because there should only ever be one object per camera
-    private static T265Camera slamra = null;
+    T265Camera slamera;
 
-    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+    //private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     @Override
-    public void init() {
-        if (slamra == null) {
-            slamra = new T265Camera(new Transform2d(), 0.1, hardwareMap.appContext);
+    public void runOpMode() throws InterruptedException {
+        slamera = T265Helper.getCamera(
+                new T265Camera.OdometryInfo(
+                        new Pose2d(), 0.8
+                ), hardwareMap.appContext);
+
+        System.out.println("731: camera initialized");
+        // This is the transformation between the center of the camera and the center of the robot
+        Pose2d cameraToRobot = new Pose2d();
+        // Increase this value to trust encoder odometry less when fusing encoder measurements with VSLAM
+        double encoderMeasurementCovariance = 0.8;
+        // Set to the starting pose of the robot
+        Pose2d startingPose = new Pose2d();
+
+        slamera.setOdometryInfo(cameraToRobot, encoderMeasurementCovariance);
+
+        slamera.setPose(startingPose); // Useful if your robot doesn't start at the field-relative origin
+
+        while (opModeIsActive()) {
+            telemetry.addData("x", slamera.getLastReceivedCameraUpdate().pose.getX());
+            telemetry.addData("y", slamera.getLastReceivedCameraUpdate().pose.getY());
+            telemetry.addData("heading", slamera.getLastReceivedCameraUpdate().pose.getHeading());
+
+            telemetry.update();
         }
     }
-
-    @Override
-    public void init_loop() {
-    }
-
-    @Override
-    public void start() {
-        slamra.start();
-    }
-
-    @Override
-    public void loop() {
-        final int robotRadius = 9; // inches
-
-        TelemetryPacket packet = new TelemetryPacket();
-        Canvas field = packet.fieldOverlay();
-
-        T265Camera.CameraUpdate up = slamra.getLastReceivedCameraUpdate();
-        if (up == null) return;
-
-        // We divide by 0.0254 to convert meters to inches
-        Translation2d translation = new Translation2d(up.pose.getTranslation().getX() / 0.0254, up.pose.getTranslation().getY() / 0.0254);
-        Rotation2d rotation = up.pose.getRotation();
-
-        field.strokeCircle(translation.getX(), translation.getY(), robotRadius);
-        double arrowX = rotation.getCos() * robotRadius, arrowY = rotation.getSin() * robotRadius;
-        double x1 = translation.getX() + arrowX  / 2, y1 = translation.getY() + arrowY / 2;
-        double x2 = translation.getX() + arrowX, y2 = translation.getY() + arrowY;
-        field.strokeLine(x1, y1, x2, y2);
-
-        dashboard.sendTelemetryPacket(packet);
-    }
-
-    @Override
-    public void stop() {
-        slamra.stop();
-    }
-
 }
