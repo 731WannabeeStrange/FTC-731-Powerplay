44764425a9eb6a2bc72a8a0f540c54390f19b8cd
Ari Singh <arisingh8@gmail.com>
2023-01-15 18:51:20 -0500

    ftc265 installed, camera is on

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/T265TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/T265TestOpMode.java
@@ -1,45 +1,42 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Transform2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.spartronics4915.lib.T265Camera;
-import com.spartronics4915.lib.T265Helper;
-
 
 @TeleOp
 @Config
 public class T265TestOpMode extends LinearOpMode
 {
-    T265Camera slamera;
+    // This is the transformation between the center of the camera and the center of the robot
+    Transform2d cameraToRobot = new Transform2d();
+    // Increase this value to trust encoder odometry less when fusing encoder measurements with VSLAM
+    double encoderMeasurementCovariance = 0.8;
+    // Set to the starting pose of the robot
+    Pose2d startingPose = new Pose2d(0, 0, new Rotation2d());
 
     //private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     @Override
     public void runOpMode() throws InterruptedException {
-        slamera = T265Helper.getCamera(
-                new T265Camera.OdometryInfo(
-                        new Pose2d(), 0.8
-                ), hardwareMap.appContext);
+        T265Camera slamra = new T265Camera(cameraToRobot, encoderMeasurementCovariance, hardwareMap.appContext);
+        slamra.setPose(startingPose); // Useful if your robot doesn't start at the field-relative origin
 
-        System.out.println("731: camera initialized");
-        // This is the transformation between the center of the camera and the center of the robot
-        Pose2d cameraToRobot = new Pose2d();
-        // Increase this value to trust encoder odometry less when fusing encoder measurements with VSLAM
-        double encoderMeasurementCovariance = 0.8;
-        // Set to the starting pose of the robot
-        Pose2d startingPose = new Pose2d();
+        // Call this when you're ready to get camera updates
+        slamra.start();
 
-        slamera.setOdometryInfo(cameraToRobot, encoderMeasurementCovariance);
-
-        slamera.setPose(startingPose); // Useful if your robot doesn't start at the field-relative origin
-
+        waitForStart();
         while (opModeIsActive()) {
-            telemetry.addData("x", slamera.getLastReceivedCameraUpdate().pose.getX());
-            telemetry.addData("y", slamera.getLastReceivedCameraUpdate().pose.getY());
-            telemetry.addData("heading", slamera.getLastReceivedCameraUpdate().pose.getHeading());
-
+            T265Camera.CameraUpdate latestUpdate = slamra.getLastReceivedCameraUpdate();
+            Pose2d latestPose = latestUpdate.pose;
+            telemetry.addData("x", latestPose.getX());
+            telemetry.addData("y", latestPose.getY());
+            telemetry.addData("heading", Math.toDegrees(latestPose.getHeading()));
+            telemetry.addData("confidence", latestUpdate.confidence);
             telemetry.update();
         }
     }
