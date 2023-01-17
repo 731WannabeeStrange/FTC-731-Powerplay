7d301879da2b359c28bbf72c98b8796c9af949b0
Keval <kevals2005@gmail.com>
2023-01-17 14:43:50 -0500

    Create IntakeTestOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/IntakeTestOpMode.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+@Config
+public class IntakeTestOpMode extends LinearOpMode
+{
+    private DcMotorEx intake1, intake2;
+
+    private double slidePower = 0;
+
+    @Override
+    public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        intake1 = hardwareMap.get(DcMotorEx.class, "intake1");
+        intake2 = hardwareMap.get(DcMotorEx.class, "intake2");
+
+        intake1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intake1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intake2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intake1.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.update();
+
+        //Wait for the start button to be pressed.
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        while (opModeIsActive()) {
+            slidePower = gamepad1.right_trigger - gamepad1.left_trigger;
+            intake1.setPower(slidePower);
+            intake2.setPower(slidePower);
+
+            telemetry.addData("intake1 position", intake1.getCurrentPosition());
+            telemetry.addData("intake2 position", intake2.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
