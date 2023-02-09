df5d3adba965490774d911a95df92b7f66495e7a
Ari Singh <arisingh8@gmail.com>
2023-02-09 09:33:18 -0500

    fix FollowTrajectory isFinished()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/commandbased/commands/FollowTrajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/commandbased/commands/FollowTrajectory.java
@@ -22,6 +22,6 @@ public class FollowTrajectory extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return driveSubsystem.isBusy();
+        return !driveSubsystem.isBusy();
     }
 }
